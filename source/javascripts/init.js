16ce21d4982ca9b134a50558027ecd6b2f2b4a2e
aaslordhunter <ali@scvngr.com>
2014-06-28 09:16:23 -0400

    wip

--- source/javascripts/init.js
+++ source/javascripts/init.js
@@ -1,13 +1,15 @@
-$('.js-fade-in').each(function(i){
-  var fadeIn = $(this);
-  
-  setTimeout(function() {
-    fadeIn.addClass('opacity-1');
-  }, 500*i);
-});
+$(window).load(function() {
+  $('.js-fade-in').each(function(i){
+    var fadeIn = $(this);
 
-$('.js-timeline').height($('.js-main').outerHeight(true));
+    setTimeout(function() {
+      fadeIn.addClass('opacity-1');
+    }, 500*i);
+  });
 
-$(window).resize(function () {
   $('.js-timeline').height($('.js-main').outerHeight(true));
+
+  $(window).resize(function () {
+    $('.js-timeline').height($('.js-main').outerHeight(true));
+  });
 });
