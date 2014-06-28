c324615da818978c4c795b007dec8506c1d63515
aaslordhunter <ali@scvngr.com>
2014-06-28 10:43:27 -0400

    wip

--- source/javascripts/init.js
+++ source/javascripts/init.js
@@ -1,11 +1,18 @@
-$(window).load(function() {
-  $('.js-fade-in').each(function(i){
-    var fadeIn = $(this);
+var onMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-    setTimeout(function() {
-      fadeIn.addClass('opacity-1');
-    }, 500*i);
-  });
+$(window).load(function() {
+  if (onMobileDevice) {
+    $('.js-fade-in').show();
+
+  } else {
+    $('.js-fade-in').each(function(i){
+      var fadeIn = $(this);
+
+      setTimeout(function() {
+        fadeIn.addClass('opacity-1');
+      }, 500*i);
+    });
+  };
 
   $('.js-timeline').height($('.js-main').outerHeight(true));
 
